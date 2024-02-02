a08450711d84ba982f48bc40440a6f8635378f5a
Michel Henrique <michelstcosta@gmail.com>
2024-02-02 18:23:57 -0300

    Colocar todos os ep de cada temporada

--- src/main/java/br/com/alura/scrrenmatch/ScrrenmatchApplication.java
+++ src/main/java/br/com/alura/scrrenmatch/ScrrenmatchApplication.java
@@ -1,11 +1,16 @@
 package br.com.alura.scrrenmatch;
 
+import br.com.alura.scrrenmatch.model.DadosEpisodio;
 import br.com.alura.scrrenmatch.model.DadosSerie;
+import br.com.alura.scrrenmatch.model.DadosTemporada;
 import br.com.alura.scrrenmatch.service.ConsumoApi;
 import br.com.alura.scrrenmatch.service.ConverterDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScrrenmatchApplication implements CommandLineRunner {
@@ -25,5 +30,23 @@ public class ScrrenmatchApplication implements CommandLineRunner {
 		System.out.println("**************************");
 		System.out.println(dados);
 		System.out.println("**************************");
+
+		json = consumoApi.obterDados("https://www.omdbapi.com/?t=teen+wolf&season=3&episode=3&apikey=d343fc07");
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
+		System.out.println("**************************");
+//**********************************************************************************************************************
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for(int i =1 ; i<= dados.totalTemporadas(); i++){
+			json = consumoApi.obterDados("https://www.omdbapi.com/?t=teen+wolf&season="+i+"&apikey=d343fc07");
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
+
+
+
 	}
 }
